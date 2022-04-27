dfdd7adea16cf8bee4a7e3693379427e0df088e7
Sarome5 <mkainz.zach@gmail.com>
2022-04-28 00:36:23 +0200

    version 0.0.2

--- __main.py
+++ __main.py
@@ -1,1 +1,23 @@
-import plc_comunication
+import socket
+import pickle
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('localhost', 1234))
+server_socket.listen()
+
+def encode(packet):
+    data = b""
+    data += packet
+    values = pickle.loads(data)
+    return values
+
+data = b""
+while True:
+    connection, client_addr = server_socket.accept()
+    print('jetzt hamma eine connection, und zwar von', client_addr)
+    while True:
+
+        packet = connection.recv(200)
+        daten=encode(packet)
+
+        print(daten)
